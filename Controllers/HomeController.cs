ca059cfac8d5373980089383f9b85828fcb43fa4
m-holovan <nikita2003n@gmail.com>
2023-04-13 01:03:49 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthorizationPage.Models;
+
+namespace AuthorizationPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public IActionResult Index() => View();
+    }
+}
---
+++ Models/RegisterModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthorizationPage.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Email is requierd")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords are not the same!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
